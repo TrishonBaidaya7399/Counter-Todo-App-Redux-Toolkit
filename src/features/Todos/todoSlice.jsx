179b458f25198c1d7a24505cb3ffce85d08b5889
TrishonBaidaya7399 <githubtrishon@gmail.com>
2024-02-08 21:24:20 +0600

    updated!

--- src/features/Todos/todoSlice.jsx
+++ src/features/Todos/todoSlice.jsx
@@ -13,31 +13,30 @@ When the fetchUserData thunk is dispatched, it will trigger the pending case fir
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const fetchTodos = createAsyncThunk("todo/fetchTodos", async () => {
-  const response = await axios.get(
-    "https://jsonplaceholder.typicode.com/todos"
-  );
+export const fetchTodos = createAsyncThunk("todo.fetchTodos", async()=>{
+    const res = await axios.get("https://jsonplaceholder.typicode.com/todos")
+    return res.data;
+})
 
-  return response.data;
-});
+export const TodoSlice = createSlice({
+name: "Todo",
+initialState : {isLoading: true, todo:[], error: ""},
+reducers:{},
+extraReducers: (builder)=>{
+    builder.addCase(fetchTodos.pending, (state)=>{
+        state.isLoading = true
+    })
+    builder.addCase(fetchTodos.fulfilled, (state, action)=>{
+        state.isLoading = false,
+        state.todo = action.payload
+    })
+    builder.addCase(fetchTodos.rejected, (state, action)=>{
+        state.isLoading = false,
+        state.error= action.error
+    })
+}
 
-export const todoSlice = createSlice({
-  name: "todo",
-  initialState: { todo: [], isLoading: true, error: "" },
-  reducers: {},
-  extraReducers: (builder) => {
-    builder.addCase(fetchTodos.pending, (state) => {
-      state.isLoading = true;
-    });
-    builder.addCase(fetchTodos.fulfilled, (state, action) => {
-      state.isLoading = false;
-      state.todo = action.payload;
-    });
-    builder.addCase(fetchTodos.rejected, (state, action) => {
-      state.isLoading = false;
-      state.error = action.error.message;
-    });
-  },
-});
+})
 
-export default todoSlice.reducer;
+export default TodoSlice.reducer;
+
